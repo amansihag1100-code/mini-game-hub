dffc2703ea5fd0ed39cf095997968b2fe55ff022
Akshit-Anand-008 <akshitanand008@gmail.com>
2026-04-05 12:33:02 +0530

    remove gpt code

--- games/connect4.py
+++ games/connect4.py
@@ -2,7 +2,7 @@ import pygame as pg
 import numpy as np
 from sys import exit
 
-#pygame
+# pygame
 pg.init()
 clock = pg.time.Clock()
 space = 1000
@@ -10,36 +10,38 @@ screen_height = space
 screen_width = space
 screen = pg.display.set_mode((screen_width, screen_height))
 
-#general
+# general
 dim = 7
 len = 4
-ptr = int(dim/2)
-board = np.full((dim,dim), -1)
+ptr = int(dim / 2)
+board = np.full((dim, dim), -1)
 height = np.zeros(dim)
 turn = 1
 winner = -1
 game_active = True
-box_len = space/(dim+2)
+box_len = space / (dim + 2)
 pgfont = pg.font.Font(None, int(box_len))
 thickness = 20
-x_surf = pgfont.render("X", False, "Lime" )
+x_surf = pgfont.render("X", False, "Lime")
 x_rect = x_surf.get_rect
-o_surf = pgfont.render("O", False, "Pink" )
+o_surf = pgfont.render("O", False, "Pink")
 
-#assets
-symbol = {0:"O", 1:"X"}
-skeleton = pg.Rect(box_len, box_len, dim*box_len, dim*box_len)
+# assets
+symbol = {0: "O", 1: "X"}
+skeleton = pg.Rect(box_len, box_len, dim * box_len, dim * box_len)
 
-#functions
+
+# functions
 def toggle_turn():
     global turn
     turn = 1 - turn
+
 
 def reset():
     board.fill(-1)
     height.fill(0)
     global ptr
-    ptr = int(dim/2)
+    ptr = int(dim / 2)
     global turn
     turn = 1
     global winner
@@ -47,21 +49,26 @@ def reset():
     global game_active
     game_active = True
 
-def examine(len, x, y, dx,  dy) -> int:
+
+def examine(len, x, y, dx, dy) -> int:
     val = board[y][x]
     for i in range(len):
-        if board[y + ( i*dy )][x + ( i*dx )] == val:
+        if board[y + (i * dy)][x + (i * dx)] == val:
             continue
         else:
             return -1
     return int(val)
 
+
 def check_winner():
     global winner
-    for [dx, dy] in [(0,1), (1,0), (1,1), (-1,1)]:
+    for [dx, dy] in [(0, 1), (1, 0), (1, 1), (-1, 1)]:
         for x in range(dim):
             for y in range(dim):
-                if ( 0<=(x+(dx*(len-1))) < dim and 0 <=(y+(dy*(len-1)))< dim ):
+                if (
+                    0 <= (x + (dx * (len - 1))) < dim
+                    and 0 <= (y + (dy * (len - 1))) < dim
+                ):
                     if examine(len, x, y, dx, dy) != -1:
                         return examine(len, x, y, dx, dy)
 
@@ -73,38 +80,50 @@ def check_winner():
     return 2
 
 
-#game loop
+# game loop
 while True:
-    #screen
+    # screen
     screen.fill("black")
     pg.draw.rect(screen, "white", skeleton, 1)
-    for x in range(dim-1):
-        pg.draw.line(screen, "White", ( box_len*(2+x), box_len ), ( box_len*(2+x), box_len*(dim+1) ), width=1)
-    for y in range(dim-1):
-        pg.draw.line(screen, "White", ( box_len, box_len*(2+y) ), ( box_len*(dim+1), box_len*(2+y) ), width=1)
+    for x in range(dim - 1):
+        pg.draw.line(
+            screen,
+            "White",
+            (box_len * (2 + x), box_len),
+            (box_len * (2 + x), box_len * (dim + 1)),
+            width=1,
+        )
+    for y in range(dim - 1):
+        pg.draw.line(
+            screen,
+            "White",
+            (box_len, box_len * (2 + y)),
+            (box_len * (dim + 1), box_len * (2 + y)),
+            width=1,
+        )
 
-    #Drawing X and O
+    # Drawing X and O
     for x in range(dim):
         for y in range(dim):
-            if board[(y,x)] == 0:
-                screen.blit(o_surf ,( box_len*(1.27 + x), box_len*(1.2 + y) ) )
-            if board[(y,x)] == 1:
-                screen.blit(x_surf ,( box_len*(1.27 + x), box_len*(1.2 + y) ) )
+            if board[(y, x)] == 0:
+                screen.blit(o_surf, (box_len * (1.27 + x), box_len * (1.2 + y)))
+            if board[(y, x)] == 1:
+                screen.blit(x_surf, (box_len * (1.27 + x), box_len * (1.2 + y)))
 
-    #event loop
+    # event loop
     for event in pg.event.get():
-        #Quit
+        # Quit
         if event.type == pg.QUIT:
             pg.quit()
             exit()
-        #Keyboard input
+        # Keyboard input
         elif event.type == pg.KEYDOWN:
-            #Filling board and checking winner
+            # Filling board and checking winner
             if event.key == pg.K_RETURN or event.key == pg.K_e:
-                if game_active == False:
+                if not game_active:
                     reset()
-                elif game_active == True and height[ptr] < dim:
-                    board[int(dim-height[ptr]-1)][ptr] = turn
+                elif game_active and height[ptr] < dim:
+                    board[int(dim - height[ptr] - 1)][ptr] = turn
                     height[ptr] += 1
                     toggle_turn()
                     winner = check_winner()
@@ -112,22 +131,22 @@ while True:
                         continue
                     else:
                         game_active = False
-            #Quit using q
+            # Quit using q
             elif event.key == pg.K_q:
                 pg.quit()
                 exit()
-            #Reset using r
+            # Reset using r
             elif event.key == pg.K_r:
                 reset()
-            #Movement Keys
+            # Movement Keys
             elif event.key == pg.K_LEFT or event.key == pg.K_a:
-                ptr = max(0, ptr-1)
+                ptr = max(0, ptr - 1)
             elif event.key == pg.K_RIGHT or event.key == pg.K_d:
-                ptr = min(dim-1, ptr+1)
+                ptr = min(dim - 1, ptr + 1)
 
     if game_active:
         menu_surf = pgfont.render(f"{symbol[turn]}'s turn", False, "White")
-        cursor = pg.Rect(box_len*(ptr+1), box_len-thickness, box_len, thickness)
+        cursor = pg.Rect(box_len * (ptr + 1), box_len - thickness, box_len, thickness)
         pg.draw.rect(screen, "yellow", cursor, 0)
 
     else:
@@ -136,9 +155,9 @@ while True:
         else:
             menu_surf = pgfont.render(f"{symbol[winner]} won", False, "Yellow")
 
-    menu_rect = menu_surf.get_rect(center = ( space/2, box_len/2 ) )
+    menu_rect = menu_surf.get_rect(center=(space / 2, box_len / 2))
     screen.blit(menu_surf, menu_rect)
 
-    #End
+    # End
     pg.display.update()
     clock.tick(16)
