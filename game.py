b49b6d8e4cf4c44f060d5ba624b1b3b8e64729a1
Aman_Sihag <amansihag1100@gmail.com>
2026-04-13 06:55:49 +0000

    made game menu some more better

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+
 import pygame as py    #alias
 import sys             # to read command line argument , exit program safely
 import os                # used for file handling , history.csv
@@ -10,22 +11,65 @@ player2 = sys.argv[2]  # get usernames from main.sh , python3 game.py aman ankit
 py.init()  # starts all pygame modules
 
 
+
+
 # creating window
 width,height = 600,500
 screen = py.display.set_mode((width,height))         #set_mode creates window
 py.display.set_caption("MINI GAME HUB")
 
+#image as background
+
+bgimg = py.image.load("materials/bgimg.jpg")
+bgimg = py.transform.scale(bgimg,(width,height))
+
+
 font = py.font.SysFont(None,40) #to set font of none type and size 40
 
+textcolor = (255,255,255)
+
 #how to write text inside buttons or to draw text and put it at x,y pos
-def  draw_text(text,x,y):
-    img = font.render(text,True,black)  #font render converts text to image
-    screen.blit(img,(x,y))     # put that img at x,y position on screen
+def  draw_text(text,size,x,y,color=textcolor):
+    font = py.font.SysFont("arial",size,bold=True)
+    img = font.render(text,True,color)  #font render converts text to image
+    rect = img.get_rect(center=(x,y))
+    screen.blit(img,rect)     # put that img at x,y position on screen
 
 
 white = (255,255,255)   # NEED TO DEFINE COLOR BEFORE NO COLOR IS IMPORTED
-black = (0,0,0)
-blue = (50 , 150 ,255)
+black = (30,30,30)
+hovercolor = (100 , 170 ,255)
+textcolor = (255,255,255)
+btncolor = (70,130,255)
+
+backtop = (30,30,60)
+backbtm = (10,10,30)
+
+
+#button with hover effect
+def drawbutton(rect,text):
+    mouseposition = py.mouse.get_pos()
+
+    if rect.collidepoint(mouseposition):
+        py.draw.rect(screen,hovercolor,rect,border_radius=15)
+    
+    else:
+        py.draw.rect(screen , btncolor,rect,border_radius=15)
+
+    draw_text(text,25,rect.centerx,rect.centery)
+
+
+#function for background with gradient color
+def gradient():
+    for y in range(height):
+        color = ( backtop[0] + (backbtm[0] - backtop[0]) * y//height ,
+                  backtop[1] + (backbtm[1] - backtop[1]) * y // height,
+                  backtop[2] +(backbtm[2] - backtop[2]) * y//height)
+
+        py.draw.line(screen,color,(0,y),(width,y))
+
+
+
 
 # function to run game
 
@@ -49,6 +93,25 @@ running = True
 while running:
     screen.fill(white)    #everytime in each loop frame make screen white
 
+    screen.blit(bgimg,(0,0))    #put img at (0,0)
+
+
+    draw_text("MINI GAME HUB",45,width//2,80)
+
+    draw_text(f"{player1} Vs {player2}" , 25 , width//2 ,130)
+
+
+
+    ttt_btn = py.Rect(200,150,200,50)
+    oth_btn = py.Rect(200,230,200,50)    #x,y and w,h of clickable box
+    c4_btn = py.Rect(200,310,200,50)
+
+
+    drawbutton(ttt_btn,"TIC TAC TOE")
+    drawbutton(oth_btn,"OTHELLO")
+    drawbutton(c4_btn,"CONNECT 4")
+
+
     for event in py.event.get():      #takes event mouse clicks , keyboard , window close etc.
         if event.type == py.QUIT:          #user clicks cross
             running = False
@@ -67,14 +130,13 @@ while running:
                 launchgame("games/connect4.py")
 
 
+
+    #gradient()
+
     
-    draw_text("MINI GAME HUB",185,50)
-
-    ttt_btn = py.Rect(200,150,200,50)
-    oth_btn = py.Rect(200,230,200,50)    #x,y and w,h of clickable box
-    c4_btn = py.Rect(200,310,200,50)
-
-    py.draw.rect(screen,blue,ttt_btn)
+    
+    '''
+     py.draw.rect(screen,blue,ttt_btn)
     py.draw.rect(screen,blue,oth_btn)
     py.draw.rect(screen,blue,c4_btn)
 
@@ -82,8 +144,9 @@ while running:
     # text to be written inside buttons
     draw_text("TIC TAC TOE",218,162)
     draw_text("OTHELLO",235,242)
-    draw_text("CONNECT 4",225,322)
+    draw_text("CONNECT 4",225,322)'''
 
+    
 
     py.display.update()          #refresh screen 
 
@@ -92,4 +155,6 @@ while running:
 
 py.quit()   #close pygame
 sys.exit()        #exit program
+
+
 
